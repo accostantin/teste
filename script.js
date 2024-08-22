a9c42689a3950636030bf28480e1ddbc835ed986
Ana Costantin Morel <anaclaracmorel@gmail.com>
2024-08-22 09:23:41 -0300

    site grid assim que acaba

---
+++ script.js
@@ -0,0 +1,7 @@
+//alerta na tela
+alert("aviso!!");
+//console do navegador
+console.log("teste de console");
+//inclui um html elemento identificado pelo id
+document.getElementById("demo").innerHTML= "sandro";
+//
